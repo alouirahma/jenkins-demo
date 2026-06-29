23765d8d2cc00b9c01686dc86992b301e44c32e8
alouirahma <rahma.aloui@esprit.tn>
2026-06-29 15:16:49 +0100

    feat: Ajout de UserService

--- src/main/java/com/example/UserService.java
+++ src/main/java/com/example/UserService.java
@@ -1,17 +1,18 @@
-package com.example;
+﻿package com.example;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class UserService {
     private final Map<Integer, String> users = new HashMap<>();
-
+    
     public UserService() {
         users.put(1, "Alice");
         users.put(2, "Bob");
         users.put(3, "Charlie");
     }
-
+    
     public String getUserName(int id) {
         return users.getOrDefault(id, "Inconnu");
     }
+}
