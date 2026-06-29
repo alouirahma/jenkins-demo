d7723ce995b574f6cfeb3a32274cc9232bde017a
selimhedi6 <selimhedi6@gmail.com>
2026-06-29 16:15:25 +0100

    feat: Ajout de la méthode square() et mise à jour App

--- Desktop/merge/demandeGestion/src/main/java/com/example/App.java
+++ Desktop/merge/demandeGestion/src/main/java/com/example/App.java
@@ -2,9 +2,10 @@
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("🚀 Application DevMedic - " + System.getProperty("user.name"));
+        System.out.println("🚀 Application DevMedic v2.0");
         Calculator calc = new Calculator();
         System.out.println("10 + 20 = " + calc.add(10, 20));
         System.out.println("5 * 6 = " + calc.multiply(5, 6));
+        System.out.println("4^2 = " + calc.square(4));
     }
 }
